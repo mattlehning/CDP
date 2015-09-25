f19404f3520ea77ee4a081277fb540078e4c9046
Matt Lehning <matt@duckduckgo.com>
2015-09-25 07:26:52 -0400

    Updated
    Updated the code.

--- CDP.c
+++ CDP.c
@@ -1,7 +1,35 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1343\cocoasubrtf140
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural
+int PB;
+int read;
 
-\f0\fs24 \cf0 A}
+void setup() {
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(12, INPUT);
+}
+
+void loop() {
+  
+  PB = digitalRead(12);
+  read = 2;
+  
+  if(read==2 && PB)
+  {
+    read = 0;
+  }
+  //foward
+  if(read==0 && PB)
+  {
+    digitalWrite(7, HIGH);
+    digitalWrite(8, LOW);
+    read = 1;
+    delay(1300);
+  }
+  //backward
+  if(read==1 && PB)
+  {
+    digitalWrite(7, LOW);
+    digitalWrite(8, HIGH);
+    read = 0;
+    delay(1300);
+  }
+}
